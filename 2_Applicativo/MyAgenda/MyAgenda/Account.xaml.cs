3e6e8fcc4c0b9d69d4563489e91e152ba562ae1a
David <uzf668@student.edu.ti.ch>
2026-05-08 16:28:24 +0200

    Aggiornamenti

--- 2_Applicativo/MyAgenda/MyAgenda/Account.xaml.cs
+++ 2_Applicativo/MyAgenda/MyAgenda/Account.xaml.cs
@@ -3,20 +3,9 @@ namespace MyAgenda;
 
 public partial class Account : ContentPage
 {
-    string[] profilePictures = new string[]
-    {
-        "melodie.png",
-        "superteo08_logo.png",
-        "viggo2.png",
-        "juggy.png",
-        "haku.png",
-        "st08brawl.png",
-        "girlfriend.jpeg"
-    };
-    public Account()
+	public Account()
 	{
         InitializeComponent();
-
 	}
     private async void OnHomeClicked(object sender, EventArgs e)
     {
