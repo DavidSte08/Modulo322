c2c18c1213a6c8245327ea9adea71e552f17af65
MateoZrakic <zrakicmateo@gmail.com>
2026-05-08 16:08:33 +0200

    Home Page e Materie

---
+++ 2_Applicativo/Mateo/MyAgenda/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MyAgenda
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Registra tutte le tue pagine
+            Routing.RegisterRoute("Account", typeof(Account));
+            Routing.RegisterRoute("HomePage", typeof(HomePage));
+            Routing.RegisterRoute("Attivita", typeof(Attivita));
+            Routing.RegisterRoute("Scuola", typeof(Scuola));
+            Routing.RegisterRoute("Login", typeof(Login));
+        }
+    }
+}
